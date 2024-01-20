7420e135e1d04c483045e5bc7aeafb7c0434ae6b
Alex Russell <alexrusselldev@gmail.com>
2024-01-20 15:31:59 +0000

    feat: ✨ add function for getting existing pages slugs

--- src/scripts/new-page.ts
+++ src/scripts/new-page.ts
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-function loadTemplate() {
+function loadTemplate(path: string) {
   let template;
   try {
-    template = fs.readFileSync(`${process.cwd()}/src/templates/page.hbs`);
+    template = fs.readFileSync(path);
   } catch (e: any) {
     if (e?.code == 'ENOENT') {
       console.log('Page template file missing.');
@@ -17,5 +17,32 @@ function loadTemplate() {
   return template;
 }
 
-const template = loadTemplate();
-console.log(template);
+function getExistingSlugs(path: string): string[] {
+  let files;
+  try {
+    files = fs.readdirSync(path);
+  } catch (e: any) {
+    if (e?.code == 'ENOENT') {
+      console.log('Content directory missing.');
+      process.exit(1);
+    }
+
+    console.log('Encountered an unknown error loading the template file.');
+    process.exit(1);
+  }
+  const returnValue = files
+    .filter((file: string) => {
+      if (file == 'index.mdx') return false;
+      if (file.slice(file.length - 3) != 'mdx') return false;
+      return true;
+    })
+    .map((file: string) => {
+      return file.substring(0, file.length - 4);
+    });
+
+  return returnValue;
+}
+
+const template = loadTemplate(`${process.cwd()}/src/templates/page.hbs`);
+const existingPages = getExistingSlugs(`${process.cwd()}/src/content`);
+console.log(existingPages);
