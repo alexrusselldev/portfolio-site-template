49d6bda62db230c85a45f4c74ac40e852ca421ae
Alex Russell <alexrusselldev@gmail.com>
2024-01-21 12:30:00 +0000

    feat: ✨ get array representing nav order for pages

--- src/scripts/new-page.ts
+++ src/scripts/new-page.ts
@@ -1,5 +1,7 @@
 import { loadTemplate, getExistingSlugs, slugify, populateFrontmatter, buildFile, writeFile } from './functions';
 import { PromptObject } from 'prompts';
+import fs, { PathLike } from 'fs';
+import { readMDX } from '8==D/lib/mdx';
 
 const pageConfig: PromptObject[] = [
   {
@@ -72,10 +74,77 @@ const pageConfig: PromptObject[] = [
   },
 ];
 
+async function getNavOrder(path: PathLike) {
+  let files;
+  try {
+    files = fs.readdirSync(path);
+  } catch (e: any) {
+    if (e?.code == 'ENOENT') {
+      console.log('Content directory missing.');
+      process.exit(1);
+    }
+
+    console.log('Encountered an unknown error loading the template file.');
+    process.exit(1);
+  }
+
+  const fileNames = files.filter((file: string) => {
+    if (file.slice(file.length - 3) != 'mdx') return false;
+    return true;
+  });
+
+  const frontMatter = await Promise.all(
+    fileNames.map(async (fileName) => {
+      const file = await readMDX(`${path}/${fileName}`);
+      if (file.frontmatter) {
+        return {
+          fileName,
+          frontMatter: file.frontmatter,
+        };
+      }
+      return file?.frontmatter || null;
+    }),
+  );
+
+  const navOrder = frontMatter
+    .filter((file) => {
+      return file?.frontMatter?.navOrder;
+    })
+    .sort((a, b) => {
+      if (a?.frontMatter?.navOrder == undefined && b?.frontMatter?.navOrder == undefined) {
+        return 0;
+      }
+
+      if (a?.frontMatter?.navOrder == undefined) {
+        return 1;
+      }
+      if (b?.frontMatter?.navOrder == undefined) {
+        return 1;
+      }
+
+      if (a.frontMatter.navOrder > b.frontMatter.navOrder) {
+        return 1;
+      }
+
+      if (a.frontMatter.navOrder < b.frontMatter.navOrder) {
+        return -1;
+      }
+
+      return 0;
+    })
+    .map((file) => {
+      return file.fileName;
+    });
+
+  return navOrder;
+}
+
 async function script() {
   const template = loadTemplate(`${process.cwd()}/src/templates/page.hbs`);
   const frontMatter = await populateFrontmatter(pageConfig);
   const file = buildFile(template, frontMatter);
+  const navOrder = await getNavOrder(`${process.cwd()}/src/content/`);
+
   writeFile(`${process.cwd()}/src/content/${frontMatter.slug}.mdx`, file);
 }
 
