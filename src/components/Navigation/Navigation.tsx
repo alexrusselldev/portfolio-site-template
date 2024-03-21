a5df264e395fd2529d3e82abd85acc3ec12d6cb7
Alex Russell <alexrusselldev@gmail.com>
2024-03-21 06:49:12 +0000

    refactor: ♻️ fetch and sort nav links in topbar component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,48 +1,18 @@
 import Link from 'next/link';
 import { getPathsFromDir, readMDX } from '@/lib/mdx';
+import { IPageNavData } from '../TopBar';
 
-interface IProps {}
-export const Navigation: React.FC<IProps> = async () => {
-  const allPagesSlugs = await getPathsFromDir(`${process.cwd()}/src/content/`);
+interface IProps {
+  pages: IPageNavData[];
+}
 
-  allPagesSlugs.push({ slug: 'index' });
-
-  const allPagesData = await Promise.all(
-    allPagesSlugs.map(async (post) => {
-      return {
-        slug: post.slug,
-        content: await readMDX(`${process.cwd()}/src/content/${post.slug}.mdx`),
-      };
-    }),
-  );
-
-  allPagesData.sort((a, b) => {
-    if (a?.content?.frontmatter?.navOrder == undefined && b?.content?.frontmatter?.navOrder == undefined) {
-      return 0;
-    }
-
-    if (a?.content?.frontmatter?.navOrder == undefined) {
-      return 1;
-    }
-    if (b?.content?.frontmatter?.navOrder == undefined) {
-      return 1;
-    }
-
-    if (a.content.frontmatter.navOrder > b.content.frontmatter.navOrder) {
-      return 1;
-    }
-
-    if (a.content.frontmatter.navOrder < b.content.frontmatter.navOrder) {
-      return -1;
-    }
-
-    return 0;
-  });
+export const Navigation: React.FC<IProps> = async (props) => {
+  const { pages } = props;
 
   return (
     <nav role="navigation">
       <ul className="container flex flex-col items-center justify-center gap-2 py-4 sm:flex-row sm:gap-4">
-        {allPagesData.map(({ slug, content }) => {
+        {pages.map(({ slug, content }) => {
           if (!content?.frontmatter?.showInNav) return null;
           return (
             <li key={slug}>
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,11 +1,55 @@
+import { MDXRemoteProps } from 'next-mdx-remote';
 import { Navigation } from '../Navigation/Navigation';
+import { getPathsFromDir, readMDX } from '@/lib/mdx';
+
+export interface IPageNavData {
+  slug: string;
+  content: MDXRemoteProps;
+}
 
 interface IProps {}
 
-export const TopBar: React.FC<IProps> = () => {
+export const TopBar: React.FC<IProps> = async () => {
+  const allPagesSlugs = await getPathsFromDir(`${process.cwd()}/src/content/`);
+
+  allPagesSlugs.push({ slug: 'index' });
+
+  const allPagesData: IPageNavData[] = await Promise.all(
+    allPagesSlugs.map(async (post) => {
+      return {
+        slug: post.slug,
+        content: await readMDX(`${process.cwd()}/src/content/${post.slug}.mdx`),
+      };
+    }),
+  );
+  console.log(allPagesData);
+
+  allPagesData.sort((a, b) => {
+    if (a?.content?.frontmatter?.navOrder == undefined && b?.content?.frontmatter?.navOrder == undefined) {
+      return 0;
+    }
+
+    if (a?.content?.frontmatter?.navOrder == undefined) {
+      return 1;
+    }
+    if (b?.content?.frontmatter?.navOrder == undefined) {
+      return 1;
+    }
+
+    if (a.content.frontmatter.navOrder > b.content.frontmatter.navOrder) {
+      return 1;
+    }
+
+    if (a.content.frontmatter.navOrder < b.content.frontmatter.navOrder) {
+      return -1;
+    }
+
+    return 0;
+  });
+
   return (
     <div className="sticky mx-auto w-full">
-      <Navigation />
+      <Navigation pages={allPagesData} />
     </div>
   );
 };
